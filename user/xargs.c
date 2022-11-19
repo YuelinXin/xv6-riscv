5898d89add576be3f9f81f3672969194f21c6c54
YuelinXin <sc20yx2@leeds.ac.uk>
2022-11-19 21:43:31 +0000

    redeem xrags.c

--- user/xargs.c
+++ user/xargs.c
@@ -1,7 +1,12 @@
 #include "kernel/types.h"
+#include "kernel/param.h"
 
 int main(int argc, char* argv[])
 {
-
+	if(argc > MAXARG){
+		fprintf(stderr, "too many aarguements\n");
+		return -1;
+	}
+	
 	return 0;
 }
