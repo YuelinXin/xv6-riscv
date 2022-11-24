ebda3e4787a3a8ed9c919852747220a1365103d2
YuelinXin <sc20yx2@leeds.ac.uk>
2022-11-24 16:16:23 +0000

    push malloc/free

--- user/memory_management.c
+++ user/memory_management.c
@@ -4,27 +4,103 @@
 #include "user/user.h"
 #include "user/memory_management.h"
 
+void *global_base = 0;
+
 // malloc
-void *malloc(int size)
+void *_malloc(int size)
 {
-    // return NULL if size is 0
-    if (size <= 0)
+    Block *block;
+    // TODO: align size?
+
+    if (size <= 0) // invalid size?
     {
-        return NULL;
+        return 0;
     }
-    // allocate memory
-    void *ptr = sbrk(size);
-    // return NULL if allocation failed
-    if (ptr == (void *)-1)
+
+    if (size > SIZE_MAX / 2) // too big?
     {
-        return NULL;
+        return 0;
     }
-    // return pointer to allocated memory
-    return ptr;
+
+    if (!global_base)
+    { // First call.
+        block = request_space(0, size);
+        if (!block)
+        {
+            return 0;
+        }
+        global_base = block;
+    }
+    else
+    {
+        Block *last = global_base;
+        block = find_free_block(&last, size);
+        if (!block)
+        { // Failed to find free block.
+            block = request_space(last, size);
+            if (!block)
+            {
+                return 0;
+            }
+        }
+        else
+        { // Found free block
+            // TODO: consider splitting block here.
+            block->free = 0;
+        }
+    }
+
+    return (block + 1);
 }
 
 // free
-void free(void *p)
+void _free(void *ptr)
 {
-    // do nothing
+    if (!ptr)
+    {
+        return;
+    }
+
+    // TODO: consider merging blocks once splitting blocks is implemented.
+    Block *block_ptr = BLOCK_ADDR(ptr);
+    if (block_ptr->free == 0)
+    {
+        block_ptr->free = 1;
+        return;
+    }
+    return;
+}
+
+Block *find_free_block(Block **last, int size)
+{
+    Block *current = global_base;
+    while (current && !(current->free && current->size >= size))
+    {
+        *last = current;
+        current = current->next;
+    }
+    return current;
+}
+
+Block *request_space(Block *last, int size)
+{
+    Block *block;
+    block = (Block *)sbrk(0); // get the top of the heap
+    void *request = sbrk(size + META_SIZE);
+    if ((void *)block == request) // Not thread safe.
+    {
+        return 0; // undefined behavior
+    }
+    if (request == (void *)-1)
+    {
+        return 0; // sbrk failed.
+    }
+    if (last)
+    { // 0 on first request.
+        last->next = block;
+    }
+    block->size = size;
+    block->next = 0;
+    block->free = 0;
+    return block;
 }
--- user/memory_management.h
+++ user/memory_management.h
@@ -1,2 +1,48 @@
+#include "kernel/types.h"
+
+#define PAGE_SIZE 4096 // the page size
+#define SIZE_MAX 0xffffffff // the maximum size of a memory block
+#define META_SIZE sizeof(Block) // the size of the metadata
+#define BLOCK_ADDR(ptr) ((Block *)ptr - 1) // the address of the metadata
+
+// a linked list of free memory blocks
+// metadata for memory partitions
+typedef struct memory_block {
+    struct memory_block *next;  // next block in the list
+    uint size;                  // size of the block
+    uint free;                  // is the block free?
+}Block;
+
+
+/**
+ * @brief Allocates a block of memory of the given size
+ * 
+ * @param size 
+ * @return void* 
+ */
 void* _malloc(int size);
+
+/**
+ * @brief: free a block of memory returned by malloc
+ *
+ * @param ptr: pointer to the block to be freed
+*/
 void _free(void* ptr);
+
+/**
+ * @brief Find a free block to accommodate the requested size
+ * 
+ * @param last 
+ * @param size 
+ * @return Block* 
+ */
+Block *find_free_block(Block **last, int size);
+
+/**
+ * @brief Request space from the OS when no free block is available
+ * 
+ * @param last 
+ * @param size 
+ * @return Block* 
+ */
+Block *request_space(Block *last, int size);
